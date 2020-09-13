b3f9ddb6b52e8796164e1e0a54c0e0b52cf1cf39
FernandezHR <fernandezhr_1998@hotmail.com>
2020-09-13 19:49:02 -0300

    Agregado campos adicionales en la ventana de agregar contacto

--- src/vista/VentanaPersona.java
+++ src/vista/VentanaPersona.java
@@ -6,6 +6,8 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JComboBox;
+import java.awt.Color;
 
 public class VentanaPersona extends JFrame 
 {
@@ -15,6 +17,10 @@ public class VentanaPersona extends JFrame
 	private JTextField txtTelefono;
 	private JButton btnAgregarPersona;
 	private static VentanaPersona INSTANCE;
+	private JTextField txtCorreo;
+	private JComboBox<String> cbxTipo;
+	private JTextField txtFecha;
+	
 	
 	public static VentanaPersona getInstance()
 	{
@@ -30,40 +36,77 @@ public class VentanaPersona extends JFrame
 	private VentanaPersona() 
 	{
 		super();
+		setTitle("Agregar Contacto");
 		
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 343, 183);
+		setBounds(100, 100, 343, 419);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(10, 11, 307, 123);
+		panel.setBounds(10, 11, 307, 359);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
-		JLabel lblNombreYApellido = new JLabel("Nombre y apellido");
-		lblNombreYApellido.setBounds(10, 11, 113, 14);
+		JLabel lblNombreYApellido = new JLabel("Nombre y apellido *");
+		lblNombreYApellido.setBounds(10, 11, 287, 14);
 		panel.add(lblNombreYApellido);
 		
-		JLabel lblTelfono = new JLabel("Telefono");
-		lblTelfono.setBounds(10, 52, 113, 14);
+		JLabel lblTelfono = new JLabel("Telefono *");
+		lblTelfono.setBounds(10, 67, 287, 14);
 		panel.add(lblTelfono);
 		
 		txtNombre = new JTextField();
-		txtNombre.setBounds(133, 8, 164, 20);
+		txtNombre.setBounds(10, 36, 287, 20);
 		panel.add(txtNombre);
 		txtNombre.setColumns(10);
 		
 		txtTelefono = new JTextField();
-		txtTelefono.setBounds(133, 49, 164, 20);
+		txtTelefono.setBounds(10, 92, 287, 20);
 		panel.add(txtTelefono);
 		txtTelefono.setColumns(10);
 		
 		btnAgregarPersona = new JButton("Agregar");
-		btnAgregarPersona.setBounds(208, 92, 89, 23);
+		btnAgregarPersona.setBounds(208, 325, 89, 23);
 		panel.add(btnAgregarPersona);
+		
+		JLabel lblCorreo = new JLabel("Correo electronico");
+		lblCorreo.setBounds(10, 123, 287, 14);
+		panel.add(lblCorreo);
+		
+		txtCorreo = new JTextField();
+		txtCorreo.setColumns(10);
+		txtCorreo.setBounds(10, 148, 287, 20);
+		panel.add(txtCorreo);
+		
+		JLabel lblTipo = new JLabel("Tipo de contacto");
+		lblTipo.setBounds(10, 179, 287, 14);
+		panel.add(lblTipo);
+		
+		cbxTipo = new JComboBox<String>();
+		cbxTipo.setName("");
+		cbxTipo.setBounds(10, 204, 149, 20);
+		cbxTipo.addItem("Trabajo");
+		cbxTipo.addItem("Familia");
+		cbxTipo.addItem("Amigos");
+		cbxTipo.addItem("Universidad");
+		panel.add(cbxTipo);
+		
+		JLabel lblFecha = new JLabel("Fecha de Cumplea\u00F1os");
+		lblFecha.setBounds(10, 235, 205, 14);
+		panel.add(lblFecha);
+		
+		JLabel lblEjFecha = new JLabel("Ej: 27/05/1998");
+		lblEjFecha.setForeground(Color.GRAY);
+		lblEjFecha.setBounds(208, 235, 89, 14);
+		panel.add(lblEjFecha);
+
+		txtFecha = new JTextField();
+		txtFecha.setColumns(10);
+		txtFecha.setBounds(10, 260, 287, 20);
+		panel.add(txtFecha);
 		
 		this.setVisible(false);
 	}
@@ -94,6 +137,5 @@ public class VentanaPersona extends JFrame
 		this.txtTelefono.setText(null);
 		this.dispose();
 	}
-	
 }
 
