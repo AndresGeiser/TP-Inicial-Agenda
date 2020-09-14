21412ba7f0786c12f557f5ead2d173f9de56e673
AndresGeiser <geiserandres00@gmail.com>
2020-09-14 09:09:39 -0300

    -Nuevos datos de persona generados

--- src/dto/PersonaDTO.java
+++ src/dto/PersonaDTO.java
@@ -5,12 +5,18 @@ public class PersonaDTO
 	private int idPersona;
 	private String nombre;
 	private String telefono;
-
-	public PersonaDTO(int idPersona, String nombre, String telefono)
+	private String correo;
+	private String tipo_contacto;
+	private String fecha_cumple;
+	
+	public PersonaDTO(int idPersona, String nombre, String telefono, String correo, String tipo_contacto, String fecha_cumple)
 	{
 		this.idPersona = idPersona;
 		this.nombre = nombre;
 		this.telefono = telefono;
+		this.correo = correo;
+		this.tipo_contacto = tipo_contacto;
+		this.fecha_cumple = fecha_cumple;
 	}
 	
 	public int getIdPersona() 
@@ -41,5 +47,29 @@ public class PersonaDTO
 	public void setTelefono(String telefono) 
 	{
 		this.telefono = telefono;
+	}
+
+	public String getCorreo() {
+		return correo;
+	}
+
+	public void setCorreo(String correo) {
+		this.correo = correo;
+	}
+
+	public String getTipo_contacto() {
+		return tipo_contacto;
+	}
+
+	public void setTipo_contacto(String tipo_contacto) {
+		this.tipo_contacto = tipo_contacto;
+	}
+
+	public String getFecha_cumple() {
+		return fecha_cumple;
+	}
+
+	public void setFecha_cumple(String fecha_cumple) {
+		this.fecha_cumple = fecha_cumple;
 	}
 }
