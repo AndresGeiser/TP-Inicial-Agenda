7032480648ce9feed0db1e83eafcd92166a104af
AndresGeiser <geiserandres00@gmail.com>
2020-09-14 08:54:38 -0300

    -Comentarios y formateo de codigo en creacion de BD

--- src/persistencia/Conexion.java
+++ src/persistencia/Conexion.java
@@ -2,7 +2,6 @@ package persistencia;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -19,11 +18,9 @@ public class Conexion
 		try
 		{
 			//Class.forName("com.mysql.jdbc.Driver"); // quitar si no es necesario
+			//Establecemos conexión con la base por defecto de MySQL y creamos la base a utilizar
 			this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","");
-			crearDB();
-			//this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/agenda?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","abc123");
-
-			//crearDB();
+			crearDB();			
 			
 			this.connection.setAutoCommit(false);
 			log.info("Conexion exitosa");
@@ -37,43 +34,22 @@ public class Conexion
 	
 	private void crearDB() throws SQLException
 	{
+		//Preparamos la sentencia
 		Statement stmt = connection.createStatement();
-
+		
 	    String db = "CREATE DATABASE IF NOT EXISTS grupo_5";
 	    String tablas = "CREATE TABLE IF NOT EXISTS personas(idPersona SERIAL NOT NULL AUTO_INCREMENT PRIMARY KEY,nombre varchar(225) NOT NULL,telefono varchar(225))";
 	    String select_db = "USE grupo_5";
 	    
-
+	    //Ejecutamos las sentencias
 	    stmt.executeUpdate(db);
-
 	    stmt.executeUpdate(select_db);
 	    
+	    //Establecemos conexión con la nueva base y creamos las tablas
 		this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/grupo_5?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","");
 
 	    stmt.executeUpdate(tablas);
 	    
-	    
-
-	    
-		/*
-		PreparedStatement statement;
-		String db = "CREATE DATABASE IF NOT EXISTS grupo_5";
-		String tablas = "CREATE TABLE IF NOT EXISTS personas(idPersona SERIAL NOT NULL AUTO_INCREMENT PRIMARY KEY,nombre varchar(225) NOT NULL,telefono varchar(225))";
-		
-		statement = this.connection.prepareStatement(db);
-
-
-		statement.executeUpdate(db);
-		
-		
-		this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/grupo_5?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC","root","");
-
-		
-		statement.executeUpdate(tablas);
-
-		
-		statement.close();
-		 */
 		log.info("BD y tablas creadas exitosamente");
 		
 	}
