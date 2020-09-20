961f89630cc7e7cd4dbe83c069b6060db3b9cf90
FernandezHR <fernandezhr_1998@hotmail.com>
2020-09-20 15:18:03 -0300

    Subido archivo con inserts de contactos para realizar pruebas

---
+++ sql/Insert-Personas.sql
@@ -0,0 +1,29 @@
+INSERT INTO `personas`(`id`, `nombre`, `telefono`, `correo`, `tipo_contacto`, `fecha_cumple`, `pais`, `provincia`, `localidad`, `calle`, `altura`, `piso`, `dpto`) 
+VALUES (0, 'Andres G.', "1154786218", "angres_geiser@gmail.com", "Universidad", "", "Argentina", "Buenos Aires","Malvinas Argentinas", "Tres Sargento", "1056", "2", "13");
+
+INSERT INTO `personas`(`id`, `nombre`, `telefono`, `correo`, `tipo_contacto`, `fecha_cumple`, `pais`, `provincia`, `localidad`, `calle`, `altura`, `piso`, `dpto`) 
+VALUES (0, 'Mama.', "1152365482", "", "Familia", "", "", "","", "", "", "", "");
+
+INSERT INTO `personas`(`id`, `nombre`, `telefono`, `correo`, `tipo_contacto`, `fecha_cumple`, `pais`, `provincia`, `localidad`, `calle`, `altura`, `piso`, `dpto`) 
+VALUES (0, 'Rodrigo Fernandez', "1166619096", "fernandezhr_1998@hotmail.com", "Universidad", "", "", "","", "", "", "", "");
+
+INSERT INTO `personas`(`id`, `nombre`, `telefono`, `correo`, `tipo_contacto`, `fecha_cumple`, `pais`, `provincia`, `localidad`, `calle`, `altura`, `piso`, `dpto`) 
+VALUES (0, 'Juancito', "1154786218", "juan_jimenez584@hotmail.com", "Amigos", "", "", "","", "", "", "", "");
+
+INSERT INTO `personas`(`id`, `nombre`, `telefono`, `correo`, `tipo_contacto`, `fecha_cumple`, `pais`, `provincia`, `localidad`, `calle`, `altura`, `piso`, `dpto`) 
+VALUES (0, 'Milagros', "1154786218", "mili2000@gmail.com", "Familia", "", "Argentina", "Buenos Aires","San Miguel", "Olegario V. Andrade", "1124", "", "");
+
+INSERT INTO `personas`(`id`, `nombre`, `telefono`, `correo`, `tipo_contacto`, `fecha_cumple`, `pais`, `provincia`, `localidad`, `calle`, `altura`, `piso`, `dpto`) 
+VALUES (0, 'Carlos Gutierrez', "1124576693", "carlitos_JM14@gmail.com", "Trabajo", "", "", "","", "", "", "", "");
+
+INSERT INTO `personas`(`id`, `nombre`, `telefono`, `correo`, `tipo_contacto`, `fecha_cumple`, `pais`, `provincia`, `localidad`, `calle`, `altura`, `piso`, `dpto`) 
+VALUES (0, 'Hermana', "1135986621", "magaliFernandez7845@gmail.com", "Familia", "", "", "","", "", "", "", "");
+
+INSERT INTO `personas`(`id`, `nombre`, `telefono`, `correo`, `tipo_contacto`, `fecha_cumple`, `pais`, `provincia`, `localidad`, `calle`, `altura`, `piso`, `dpto`) 
+VALUES (0, 'Maxi', "1154786218", "", "Amigos", "", "Uruguay", "Canelones", "Atlantida", "Muñiz", "1998", "", "");
+
+INSERT INTO `personas`(`id`, `nombre`, `telefono`, `correo`, `tipo_contacto`, `fecha_cumple`, `pais`, `provincia`, `localidad`, `calle`, `altura`, `piso`, `dpto`) 
+VALUES (0, 'Pedruski', "1147854963", "", "Universidad", "", "Chile", "Antofagasta", "Mejillones", "Lombardo", "457", "2", "13");
+
+INSERT INTO `personas`(`id`, `nombre`, `telefono`, `correo`, `tipo_contacto`, `fecha_cumple`, `pais`, `provincia`, `localidad`, `calle`, `altura`, `piso`, `dpto`) 
+VALUES (0, 'Monica S.', "1154786218", "serranomonica88@gmail.com", "Trabajo", "", "", "", "", "", "", "", "");
